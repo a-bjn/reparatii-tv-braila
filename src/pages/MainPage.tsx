61cb8a97d6ae67ce31108e6d40f9b02c2ac7eb9e
a-bjn <andreibejan55@yahoo.com>
2025-03-18 00:28:51 +0200

    add project structure

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,12 @@
+import HeroSection from "@/components/HeroSection";
+import ServiciiSection from "@/components/ServiciiSection";
+
+export default function MainPage() {
+	return (
+		<div>
+			<HeroSection/>
+			<ServiciiSection/>
+		</div>
+	);
+}
+  
