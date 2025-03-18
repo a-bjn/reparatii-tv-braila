3ed61fd60443665b398d866ee0080b4b9ec5616b
a-bjn <andreibejan55@yahoo.com>
2025-03-18 20:26:10 +0200

    add aos

--- src/components/BrandsSection.tsx
+++ src/components/BrandsSection.tsx
@@ -24,13 +24,14 @@ export default function BrandsSection() {
   return (
     <section className="relative w-full h-min-screen flex items-center justify-center bg-zinc-900 px-4 py-10">
       <div className="absolute left-0 top-0 w-full h-full bg-gradient-to-b from-black/90 to-transparent opacity-80 pointer-events-none"></div>
+      <div className="absolute left-0 bottom-0 w-full h-full bg-gradient-to-t from-black/50 to-transparent opacity-100 pointer-events-none"></div>
 
       <div className="flex flex-col items-center justify-center lg:p-12 lg:w-3/4 sm:w-full sm:p-6">
         <div className="flex flex-col items-center text-center gap-4 mt-12 mb-20" data-aos="fade-up">
           <h1 className="text-[#FFE14D] text-4xl font-bold drop-shadow-lg" data-aos="fade-up">
             Brandurile cu care lucrăm
           </h1>
-          <p className="text-white/40 text-xl font-normal" data-aos="fade-up">
+          <p className="text-white/60 text-xl font-normal" data-aos="fade-up">
             Service-ul nostru acoperă o gamă largă de televizoare de orice marcă.
           </p>
         </div>
--- src/components/ReviewsSection.tsx
+++ src/components/ReviewsSection.tsx
@@ -1,7 +1,17 @@
+"use client"
+
+
+import { useEffect } from "react";
 import { FaStar } from "react-icons/fa";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import ReviewCard from "./ReviewCard";
 
 export default function ReviewsSection() {
+  useEffect(() => {
+    AOS.init({ duration: 1000, once: true });
+  }, []);
+
   const reviews = [
     {
       name: "Lyle",
@@ -33,10 +43,10 @@ export default function ReviewsSection() {
     <section className="relative w-full h-min-screen bg-zinc-900 py-20 px-5">
       <div className="absolute left-0 top-0 w-full h-full bg-gradient-to-b from-black/60 to-transparent opacity-80 pointer-events-none"></div>
       <div className="relative flex flex-col justify-around items-center">
-        <div className="flex flex-col items-center mb-16">
+        <div className="flex flex-col items-center mb-16" data-aos="fade-up">
           <p className="text-white/45 font-thin text-4xl mb-6">Media Recenziilor</p>
           <p className="text-8xl font-thin text-white/95 mb-2">4.3/5</p>
-          <div className="flex flex-row items-center text-center gap-4">
+          <div className="flex flex-row items-center text-center gap-4" data-aos="zoom-in">
             {[...Array(4)].map((_, i) => (
               <FaStar key={i} className="text-yellow-300 text-3xl" />
             ))}
@@ -48,9 +58,12 @@ export default function ReviewsSection() {
             </div>
           </div>
         </div>
-        <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 w-full max-w-5xl">
+        <div
+          className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 w-full max-w-5xl"
+          data-aos="fade-up"
+        >
           {reviews.map((review, index) => (
-            <ReviewCard key={index} {...review} />
+            <ReviewCard key={index} {...review} data-aos="fade-up" data-aos-delay={index * 100} />
           ))}
         </div>
       </div>
