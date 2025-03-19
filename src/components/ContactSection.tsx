d0e89e0a95f8a0b819000105fd6c294466e9ae59
a-bjn <andreibejan55@yahoo.com>
2025-03-19 17:37:26 +0200

    try design

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -26,8 +26,6 @@ export default function ContactSection () {
 
   return (
     <section className="relative w-full h-min-screen bg-black/90 px-4 py-20 overflow-hidden">
-      <div className="absolute left-0 top-0 w-full h-1/3 bg-gradient-to-b from-black/80 to-transparent opacity-90 pointer-events-none"></div>
-      <div className="absolute left-0 bottom-0 w-full h-1/5 bg-gradient-to-t from-black/90 to-transparent opacity-80 pointer-events-none"></div>
       <h1 className="text-[#FFE14D] text-4xl text-center font-bold drop-shadow-lg mb-20" data-aos="fade-up">
         Contactați-ne
       </h1>
